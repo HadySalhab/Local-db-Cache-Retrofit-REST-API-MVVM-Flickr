1f591959d65974f8f7fe6a2a0571b3ea8229956a
Hady Salhab <hadysalhab1@gmail.com>
2020-01-06 15:59:32 +1100

    Adding Status variable inside the Resource class

--- app/src/main/java/com/android/flickphoto/util/Resource.kt
+++ app/src/main/java/com/android/flickphoto/util/Resource.kt
@@ -7,10 +7,11 @@ package com.android.flickphoto.util
  * @param <T>
  */
 sealed class Resource<T>(
+    val status: Status,
     val data: T? = null,
     val message: String? = null
 ) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Success<T>(data: T) : Resource<T>(Status.SUCCESS,data,null)
+    class Loading<T>(data: T? = null) : Resource<T>(Status.LOADING,data,null)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(Status.ERROR,data, message)
 }
