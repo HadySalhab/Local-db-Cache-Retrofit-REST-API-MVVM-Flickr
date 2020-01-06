4b7e2a9f73be2a8935d99472573ca8acae8dbafb
Hady Salhab <hadysalhab1@gmail.com>
2020-01-06 16:03:34 +1100

    Refactoring FlickrApi.kt to use API_KEY constant

--- app/src/main/java/com/android/flickphoto/requests/FlickrApi.kt
+++ app/src/main/java/com/android/flickphoto/requests/FlickrApi.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import com.android.flickphoto.requests.responses.ApiResponse
 import com.android.flickphoto.requests.responses.FlickrResponse
 import com.android.flickphoto.requests.responses.PhotoResponse
+import com.android.flickphoto.util.API_KEY
 import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.Response
@@ -17,7 +18,7 @@ interface FlickrApi {
 
     @GET("services/rest")
     fun searchPhotos(
-        @Query("api_key") apiKey: String = "75ec5012a10f8767d1716ce6082843de",
+        @Query("api_key") apiKey: String = API_KEY,
         @Query("method") method: String = "flickr.photos.search",
         @Query("text") text: String="",
         @Query("nojsoncallback") noJsonCallBack: String = "1",
@@ -30,7 +31,7 @@ interface FlickrApi {
 
     @GET("services/rest")
     fun getRecentPhotos(
-        @Query("api_key") apiKey: String = "75ec5012a10f8767d1716ce6082843de",
+        @Query("api_key") apiKey: String = API_KEY,
         @Query("method") method: String = "flickr.photos.getRecent",
         @Query("nojsoncallback") noJsonCallBack: String = "1",
         @Query("format") responseFormat: String = "json",
